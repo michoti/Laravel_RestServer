f1201738ce1b0e352407d877243d5e1ac02f7eb1
shukeel <50740328+shukeel@users.noreply.github.com>
2022-07-08 20:55:20 +0300

    modified Traits

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Database\Seeders\Traits\TruncateTable;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    use TruncateTable;
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->truncate('users');
+        User::factory(10)->create();
+        $this->enableForeignKeyChecks();
+    }
+}
