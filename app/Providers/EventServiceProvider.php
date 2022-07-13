fe8585fb676305c991385a32bcc214cc1794e7ec
shukeel <50740328+shukeel@users.noreply.github.com>
2022-07-13 13:33:56 +0300

    modified

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Events\UserCreatedEvent;
 use App\Listeners\SendWelcomeEmail;
+use App\Subscribers\Models\UserSubscriber;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -23,6 +24,10 @@ class EventServiceProvider extends ServiceProvider
         UserCreatedEvent::class => [
             SendWelcomeEmail::class,
         ]
+    ];
+
+    protected $subscribe = [
+        UserSubscriber::class
     ];
 
     /**
